d3e74ce864ec4e3ee4ede6c3afb86d50f13282a3
Taufiqurrahman Idrus <taufiqmck@gmail.com>
2022-10-31 05:29:40 +0800

    production push

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,6 @@
 const allowedOrigins = [
     'http://localhost:3000',
-    'https://www.dandrepairshop.com',
-    'https://dandrepairshop.com'
+    'https://cuapan.github.io'
 ]
 
 module.exports = allowedOrigins
--- config/dbConn.js
+++ config/dbConn.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.DATABASE_URI, { dbName: 'cuapan' })
+        await mongoose.connect(process.env.DATABASE_URI, { dbName: 'cuapan-prod' })
     } catch (err) {
         console.log(err)
     }
